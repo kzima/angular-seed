d1e812829e1b30428eab8475736f53abb3e16083
Kris Zima <kris.zima@crudigital.com.au>
2013-10-29 14:12:29 +1000

    added dynamic title

--- app/js/app.js
+++ app/js/app.js
@@ -9,7 +9,11 @@ angular.module('myApp', [
   'myApp.directives'
 ]).
 config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/view1', {templateUrl: 'partials/partial1.html', controller: 'MyCtrl1'});
-  $routeProvider.when('/view2', {templateUrl: 'partials/partial2.html', controller: 'MyCtrl2'});
+  $routeProvider.when('/view1', {templateUrl: 'partials/partial1.html', controller: 'MyCtrl1', title: 'My Controller 1'});
+  $routeProvider.when('/view2', {templateUrl: 'partials/partial2.html', controller: 'MyCtrl2', title: 'My Controller 2'});
   $routeProvider.otherwise({redirectTo: '/view1'});
+}]).run(['$location', '$rootScope', function($location, $rootScope) {
+  $rootScope.$on("$routeChangeSuccess", function(event, currentRoute, previousRoute) {
+    $rootScope.title = currentRoute.title;
+	});
 }]);
