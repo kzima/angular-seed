6258f647b15b8e9042fea8068f06d4d91e6bd61f
Kris Zima <kris.zima@crudigital.com.au>
2013-10-29 15:09:33 +1000

    added angular ui router for handling nested views

--- app/js/app.js
+++ app/js/app.js
@@ -3,17 +3,23 @@
 
 // Declare app level module which depends on filters, and services
 angular.module('myApp', [
-  'ngRoute',
+  'ui.router',
   'myApp.filters',
   'myApp.services',
   'myApp.directives'
 ]).
-config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/view1', {templateUrl: 'partials/partial1.html', controller: 'MyCtrl1', title: 'My Controller 1'});
-  $routeProvider.when('/view2', {templateUrl: 'partials/partial2.html', controller: 'MyCtrl2', title: 'My Controller 2'});
-  $routeProvider.otherwise({redirectTo: '/view1'});
-}]).run(['$location', '$rootScope', function($location, $rootScope) {
-  $rootScope.$on("$routeChangeSuccess", function(event, currentRoute, previousRoute) {
-    $rootScope.title = currentRoute.title;
-	});
-}]);
+config(['$stateProvider', function ($stateProvider) {
+  $stateProvider
+    .state('home', {
+      url: '/',
+      templateUrl: 'partials/partial1.html'
+    }).state('view2', {
+      url: '/',
+      templateUrl: 'partials/partial2.html'
+    })
+}]).
+run(['$rootScope', '$state', '$stateParams', function ($rootScope,   $state,   $stateParams) {
+ 	$state.transitionTo('home'); 
+  $rootScope.$state = $state;
+  $rootScope.$stateParams = $stateParams;
+}])
