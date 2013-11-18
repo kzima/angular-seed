b388781c06aed0caa8b3b39ae6140660933079f7
Kris Zima <kris@mosquito.ie>
2013-11-18 21:09:38 +1100

    cleaned up a code a bit

--- app/src/app.js
+++ app/src/app.js
@@ -9,13 +9,20 @@ angular.module( 'myApp', [
   $urlRouterProvider.otherwise( '/home' );
 })
 
-.run(['$rootScope', '$state', '$stateParams', function ($rootScope, $state, $stateParams) {
+.run([
+  '$rootScope', 
+  '$state', 
+  '$stateParams', 
+  function ($rootScope, $state, $stateParams) {
   // It's very handy to add references to $state and $stateParams to the $rootScope
   $rootScope.$state = $state;
   $rootScope.$stateParams = $stateParams;
 }])
 
-.controller( 'AppCtrl', ['$scope', '$location', function AppCtrl ( $scope, $location ) {
+.controller( 'AppCtrl', [
+  '$scope', 
+  '$location', 
+  function AppCtrl ( $scope, $location ) {
   $scope.$on('$stateChangeSuccess', function(event, toState, toParams, fromState, fromParams){
     if ( angular.isDefined( toState.data.pageTitle ) ) {
       $scope.pageTitle = toState.data.pageTitle + ' | Angular Seed' ;
